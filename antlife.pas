3789ea17b1c9c3969de1ab931e1aa39c7fc8ae7b
alipikminer <alipikminer@yandex.ru>
2016-11-20 14:23:45 +0300

    Ускорена прорисовка
    Но симуляция часто вылетает

--- antlife.pas
+++ antlife.pas
@@ -4,7 +4,7 @@ var
   antx, anty: array[1..100] of integer;
 
 var
-  map: array[1..500] of array[1..500] of integer;
+  map: array[1..500] of array[1..500] of array[1..2] of integer;
 
 var
   i, ii, x, y, t: integer;
@@ -19,7 +19,7 @@ for i := 1 to 500 do
 begin
 for ii := 1 to 500 do
 begin
-map[i][ii]:=5;  
+map[i][ii][2]:=5;  
 end;   
 end;
 //создание муравьишек
@@ -30,11 +30,11 @@ x:=random(1,500);
 y:=random(1,500);
 antx[i]:=x;
 anty[i]:=y;
-map[antx[i]][anty[i]]:=1;
-map[antx[i]+1][anty[i]]:=2;
-map[antx[i]-1][anty[i]]:=2;
-map[antx[i]][anty[i]+1]:=2;
-map[antx[i]][anty[i]-1]:=2;
+map[antx[i]][anty[i]][2]:=1;
+map[antx[i]+1][anty[i]][2]:=2;
+map[antx[i]-1][anty[i]][2]:=2;
+map[antx[i]][anty[i]+1][2]:=2;
+map[antx[i]][anty[i]-1][2]:=2;
 end;
 
 repeat
@@ -44,13 +44,17 @@ for i := 1 to 500 do
 begin
 for ii := 1 to 500 do
 begin
- case map[i][ii] of
+if map[i][ii][2]<>map[i][ii][1] then
+begin
+ case map[i][ii][2] of
 1:setpixel(i,ii,clBlack);
 2:setpixel(i,ii,clRed);
 3:setpixel(i,ii,clBrown);
 4:setpixel(i,ii,clBrown);
 5:setpixel(i,ii,clFireBrick);
 end;
+end;
+map[i][ii][1]:=map[i][ii][2];
 end;
 end;
 
@@ -66,16 +70,17 @@ end;
 //муравьи копают
 for i:=1 to 10 do
 begin
-map[antx[i]][anty[i]]:=1;
-map[antx[i]+1][anty[i]]:=2;
-map[antx[i]-1][anty[i]]:=2;
-map[antx[i]][anty[i]+1]:=2;
-map[antx[i]][anty[i]-1]:=2;
-map[antx[i]-1][anty[i]-1]:=2;
-map[antx[i]+1][anty[i]-1]:=2;
-map[antx[i]+1][anty[i]+1]:=2;
-map[antx[i]-1][anty[i]+1]:=2;
+map[antx[i]][anty[i]][2]:=1;
+map[antx[i]+1][anty[i]][2]:=2;
+map[antx[i]-1][anty[i]][2]:=2;
+map[antx[i]][anty[i]+1][2]:=2;
+map[antx[i]][anty[i]-1][2]:=2;
+map[antx[i]-1][anty[i]-1][2]:=2;
+map[antx[i]+1][anty[i]-1][2]:=2;
+map[antx[i]+1][anty[i]+1][2]:=2;
+map[antx[i]-1][anty[i]+1][2]:=2;
 end;
+
 
 until 2=3;
 end.
