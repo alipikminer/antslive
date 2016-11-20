66576ce54cacf7af7c2ae41ab600fd298978bca4
alipikminer <alipikminer@yandex.ru>
2016-11-20 17:36:51 +0300

    Рождение и смерть
    Муравьи сносят личинки перед смертью. а потом умирают. а потом
    вылупляються из личинок

--- antlife.pas
+++ antlife.pas
@@ -1,7 +1,7 @@
 uses GraphABC;
 //                 [number]
 var
-  antx, anty: array[1..100] of integer;
+  anttime, antx, anty, larvax, larvay,larvatime: array[1..100] of integer;
 
 var
   map: array[1..500] of array[1..500] of array[1..2] of integer;
@@ -13,78 +13,106 @@ var
   text: string;
 
 begin
-randomize();
-//Заполение всего экрана замлёй
-for i := 1 to 500 do
-begin
-for ii := 1 to 500 do
-begin
-map[i][ii][2]:=5;  
-end;   
-end;
-//создание муравьишек
-for i:= 1 to 10 do
-
-begin
-x:=random(1,500);
-y:=random(1,500);
-antx[i]:=x;
-anty[i]:=y;
-map[antx[i]][anty[i]][2]:=1;
-map[antx[i]+1][anty[i]][2]:=2;
-map[antx[i]-1][anty[i]][2]:=2;
-map[antx[i]][anty[i]+1][2]:=2;
-map[antx[i]][anty[i]-1][2]:=2;
-end;
-
-repeat
-
-//обновление экрана
-for i := 1 to 500 do
-begin
-for ii := 1 to 500 do
-begin
-if map[i][ii][2]<>map[i][ii][1] then
-begin
- case map[i][ii][2] of
-1:setpixel(i,ii,clBlack);
-2:setpixel(i,ii,clRed);
-3:setpixel(i,ii,clBrown);
-4:setpixel(i,ii,clBrown);
-5:setpixel(i,ii,clFireBrick);
-end;
-end;
-map[i][ii][1]:=map[i][ii][2];
-end;
-end;
-
-//муравьи идут
-for i:=1 to 10 do
-begin
-x:=random(-1,1);
-y:=random(-1,1);
-if antx[i]<=2 then if x<=0 then x:=1;
-if anty[i]<=2 then if y<=0 then y:=1;
-if antx[i]>=498 then if x>=0 then x:=-1;
-if anty[i]>=498 then if y>=0 then y:=-1;
-antx[i]:=antx[i]+x;
-anty[i]:=anty[i]+y;
-end;
-
-//муравьи копают
-for i:=1 to 10 do
-begin
-map[antx[i]][anty[i]][2]:=1;
-map[antx[i]+1][anty[i]][2]:=2;
-map[antx[i]-1][anty[i]][2]:=2;
-map[antx[i]][anty[i]+1][2]:=2;
-map[antx[i]][anty[i]-1][2]:=2;
-map[antx[i]-1][anty[i]-1][2]:=2;
-map[antx[i]+1][anty[i]-1][2]:=2;
-map[antx[i]+1][anty[i]+1][2]:=2;
-map[antx[i]-1][anty[i]+1][2]:=2;
-end;
-
-
-until 2=3;
+  randomize();
+  //Заполение всего экрана замлёй
+  for i := 1 to 500 do
+  begin
+    for ii := 1 to 500 do
+    begin
+      map[i][ii][2] := 5;  
+    end;   
+  end;
+  //создание муравьишек
+  for i := 1 to 10 do
+  
+  begin
+    x := random(1, 500);
+    y := random(1, 500);
+    antx[i] := x;
+    anty[i] := y;
+    map[antx[i]][anty[i]][2] := 1;
+    map[antx[i] + 1][anty[i]][2] := 2;
+    map[antx[i] - 1][anty[i]][2] := 2;
+    map[antx[i]][anty[i] + 1][2] := 2;
+    map[antx[i]][anty[i] - 1][2] := 2;
+    anttime[i] := random(250, 1000);
+  end;
+  
+  repeat
+    
+    //обновление экрана
+    for i := 1 to 500 do
+    begin
+      for ii := 1 to 500 do
+      begin
+        if map[i][ii][2] <> map[i][ii][1] then
+        begin
+          case map[i][ii][2] of
+            1: setpixel(i, ii, clBlack);
+            2: setpixel(i, ii, clRed);
+            3: setpixel(i, ii, clWhite);
+            4: setpixel(i, ii, clBrown);
+            5: setpixel(i, ii, clFireBrick);
+          end;
+        end;
+        map[i][ii][1] := map[i][ii][2];
+      end;
+    end;
+    
+    //муравьи идут
+    for i := 1 to 10 do
+    begin
+    if anttime[i]>0 then
+    begin
+      x := random(-1, 1);
+      y := random(-1, 1);
+      if antx[i] <= 2 then if x <= 0 then x := 1;
+      if anty[i] <= 2 then if y <= 0 then y := 1;
+      if antx[i] >= 498 then if x >= 0 then x := -1;
+      if anty[i] >= 498 then if y >= 0 then y := -1;
+      antx[i] := antx[i] + x;
+      
+      anty[i] := anty[i] + y;
+      end;
+      //личинки
+      
+      if anttime[i] > 0 then anttime[i] := anttime[i] - 1;
+     if larvatime[i] = 0 then
+     begin
+      if anttime[i] = 50 then larvax[i] := antx[i];
+      if anttime[i] = 50 then larvay[i] := anty[i];
+      if anttime[i] = 50 then larvatime[i]:=random(500,1000);
+    end;
+    
+    end;
+    
+    //муравьи копают
+    for i := 1 to 10 do
+    begin
+      map[antx[i]][anty[i]][2] := 1;
+      map[antx[i] + 1][anty[i]][2] := 2;
+      map[antx[i] - 1][anty[i]][2] := 2;
+      map[antx[i]][anty[i] + 1][2] := 2;
+      map[antx[i]][anty[i] - 1][2] := 2;
+      map[antx[i] - 1][anty[i] - 1][2] := 2;
+      map[antx[i] + 1][anty[i] - 1][2] := 2;
+      map[antx[i] + 1][anty[i] + 1][2] := 2;
+      map[antx[i] - 1][anty[i] + 1][2] := 2;
+    end;
+    
+    //личинки существуют
+    for i := 1 to 10 do
+    begin
+      if larvatime[i]>=51 then map[larvax[i]][larvay[i]][2]:=3;
+      if larvatime[i]>=51 then larvatime[i]:=larvatime[i]-1;
+      if larvatime[i]=50 then
+    begin
+      anttime[i]:=random(250,5000);
+      antx[i]:=larvax[i];
+    anty[i]:=larvay[i];
+    larvatime[i]:=0;
+    end;
+ end;
+    
+  until 2 = 3;
 end.
